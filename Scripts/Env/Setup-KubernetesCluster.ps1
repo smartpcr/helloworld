b0964e722e9ad686893a7c12b484d971c4e6b70a
xiaodoli <xiaodoli@microsoft.com>
2018-08-08 08:13:34 -0700

    "acr email"

--- Scripts/Env/Setup-KubernetesCluster.ps1
+++ Scripts/Env/Setup-KubernetesCluster.ps1
@@ -32,7 +32,7 @@ Connect-ToAzure -EnvName $EnvName -ScriptFolder $scriptFolder
 
 $aksSpnPwd = "$(az keyvault secret show --vault-name $vaultName --name $aksSpnPwdSecretName --query ""value"" -o tsv)"
 
-# this took >30 min!! Go grab a coffee.
+# this took > 30 min!! Go grab a coffee.
 az aks create `
     --resource-group $rgName `
     --name $aksClusterName `
@@ -43,29 +43,23 @@ az aks create `
     --service-principal $aksSpnAppId `
     --client-secret $aksSpnPwd
 
-az aks list -o table
-
-# if see error No module named '_cffi_backend'
-# run `brew link --overwrite python3`
-
-# retrieves kubeconfig info from cluster and merges into current kubeconfig on local machine
-# if you have vs code extension installed, you can browse into node/pod/service/rc (nice!)
-az aks get-credentials -n xdK8SCluster -g xdK8S
-
-# creates a proxy tunnel and open dashboard (note: it's using port 8001)
-az aks browse -g xdK8S -n xdK8SCluster &
-
 # grant aks read access to acr
-aksSvcPrincipalAppId="$(az aks show -g xdK8S -n xdK8SCluster --query ""servicePrincipalProfile.clientId"" -o tsv)" 
-acrId="$(az acr show -n xdcontainerregistry -g xdK8S --query ""id"" -o tsv)"
+aksSvcPrincipalAppId="$(az aks show -g $rgName -n $aksClusterName --query ""servicePrincipalProfile.clientId"" -o tsv)" 
+acrId="$(az acr show -n xdcontainerregistry -g $rgName --query ""id"" -o tsv)"
 az role assignment create --assignee $aksSvcPrincipalAppId --role Reader --scope $acrId
 
-# set AKS acr secrets
+# set acr secrets in AKS
+$acrName = $bootstrapValues.acr.name
+$acrPwdSecretName = $bootstrapValues.acr.passwordSecretName
+$acrEmail = $bootstrapValues.acr.email
+
+
 ACR_NAME="xdcontainerregistry"
 SERVICE_PRINCIPAL_NAME="xd_acr-service-principal"
 EMAIL="xiaodoli@microsoft.com"
-ACR_LOGIN_SERVER=$(az acr show -n $ACR_NAME --query loginServer -o tsv)
-ACR_REGISTRY_ID=$(az acr show -n $ACR_NAME --query id -o tsv)
+ACR_LOGIN_SERVER=$(az acr show -n $acrName --query loginServer -o tsv)
+ACR_REGISTRY_ID=$(az acr show -n $acrName --query id -o tsv)
+
 az ad sp list 
 SP_PASSWD=$(az ad sp create-for-rbac -n $SERVICE_PRINCIPAL_NAME --role Reader --scopes $ACR_REGISTRY_ID --query password -o tsv)
 CLIENT_ID=$(az ad sp show --id http://$SERVICE_PRINCIPAL_NAME --query appId -o tsv)
@@ -78,18 +72,18 @@ k create -f src/aks-helloworld/aci-helloworld.yaml
 k apply -f src/aks-helloworld/aci-helloworld.yaml 
 
 # set nodes count to desired number (this took ~10 min, super slow!!)
-az aks scale -n xdK8SCluster -g xdK8S -c 10 
-az aks scale -n xdK8SCluster -g xdK8S -c 2
+az aks scale -n $aksClusterName -g $rgName -c 10 
+az aks scale -n $aksClusterName -g $rgName -c 2
 
 # shows current control plane and agent pool version, available upgrade versions
-# az aks get-versions -n xdK8SCluster -g xdK8S --location eastus
+# az aks get-versions -n $aksClusterName -g $rgName --location eastus
 
 # upgrades
-az aks upgrade -n xdK8SCluster -g xdK8S -k 1.9.6
+az aks upgrade -n $aksClusterName -g $rgName -k 1.9.6
 
 # switch to different cluster
 k config use-context minikube
-k config use-context xdK8SCluster
+k config use-context $aksClusterName
 
 # use helm/chart (tiller service deployed to kube-system namespace)
 kubectl create -f ./helm-rbac.yaml 
