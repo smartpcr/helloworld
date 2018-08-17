333cc4f15015aeb73d5fbefc5952afe59183bc92
xiaodoli <xiaodoli@microsoft.com>
2018-08-17 14:40:30 -0700

    push image to acr

--- src/generator-api/run.ps1
+++ src/generator-api/run.ps1
@@ -17,19 +17,21 @@ Import-Module "$ScriptFolder/modules/TerraformUtil.psm1" -Force
 
 Write-Host "1) load environment yaml settings from Env/${EnvName}..." -ForegroundColor Green
 $bootstrapValues = Get-EnvironmentSettings -EnvName $EnvName -ScriptFolder $EnvFolder
-$rgName = $bootstrapValues.global.resourceGroup
 $acrName = $bootstrapValues.acr.name
-$vaultName = $bootstrapValues.kv.name 
-$acrPwdSecretName = $bootstrapValues.acr.passwordSecretName
 $spnName = $bootstrapValues.global.servicePrincipal
 
 Write-Host "2) Login to azure as service principal '$spnName' ..."
 Connect-ToAzure2 -EnvName $EnvName -ScriptFolder $EnvFolder
 
 Write-Host "3) build docker image..."
+dotnet restore generator-api.csproj 
+dotnet build generator-api.csproj
+dotnet publish generator-api.csproj
 $imageTag = "master-commitId"
-docker build -t "$acrName/generator-api:$imageTag" .
+$imageName = "generator-api"
+docker build -t "$($imageName):$($imageTag)" .
+docker tag "$($imageName):$($imageTag)" "$($acrName).azurecr.io/$($imageName):$($imageTag)"
 
 Write-Host "4) publishing image to acr..."
-az acr login -n $acrName # no longer needed
-docker push "$acrName.azureacr.io/generator-api:$imageTag"
+az acr login -n $acrName
+docker push "$($acrName).azurecr.io/$($imageName):$($imageTag)"
