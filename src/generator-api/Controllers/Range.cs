4b131b92f9af0cd24c3eb0012eb7fcbfbddd8474
xd <lingxd@gmail.com>
2018-08-16 22:15:29 -0700

    generator api

---
+++ src/generator-api/Controllers/Range.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Faker.Extensions;
+
+namespace generator_api.Controllers
+{
+    public class Range
+    {
+        public int Count { get; set; } = 10;
+        public bool Sort { get; set; } = false;
+
+        public IEnumerable<TItem> Of<TItem>(Func<TItem> generateItem)
+            => Count.Times(i => generateItem())
+                .OrderBy(n => Sort ? n : default(TItem));
+    }
+}
---
+++ src/generator-api/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace generator_api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddXmlSerializerFormatters();
+                
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Generate Random Data API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Generate Random Data API V1");
+            });
+
+            var redirectRootToSwagger = new RewriteOptions()
+                .AddRedirect("^$", "swagger");
+            app.UseRewriter(redirectRootToSwagger);
+        }
+    }
+}
