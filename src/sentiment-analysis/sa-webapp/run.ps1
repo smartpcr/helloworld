e77f0cc1159904e1f9b33456dac87db711a9bb70
xiaodoli <xiaodoli@microsoft.com>
2018-08-20 19:33:35 -0700

    need to use container ip address

--- src/sentiment-analysis/sa-webapp/run.ps1
+++ src/sentiment-analysis/sa-webapp/run.ps1
@@ -41,5 +41,7 @@ az acr login -n $acrName
 docker push "$($acrLoginServer)/$($imageName):$($imageTag)"
 
 Write-Host "4) testing..."
-docker run -d -p 8080:8080 --name $imageName "$($acrLoginServer)/$($imageName):$($imageTag)" 
+docker container stop $imageName
+docker container rm $imageName
+docker run -d -p 8080:8080 --name $imageName -e SA_LOGIC_API_URL='http://172.17.0.4:5000' "$($acrLoginServer)/$($imageName):$($imageTag)" 
 docker exec -it $imageName /bin/sh 
