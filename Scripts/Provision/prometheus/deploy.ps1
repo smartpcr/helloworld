578864d89ac20f216add46f0c28497ab22997a1f
xd <lingxd@gmail.com>
2018-08-22 00:57:17 -0700

    grafana

--- Scripts/Provision/prometheus/deploy.ps1
+++ Scripts/Provision/prometheus/deploy.ps1
@@ -3,3 +3,5 @@ kubectl create -f prometheus.yml
 kubectl create -f prometheus-resource.yml 
 kubectl create -f kubernetes-monitoring.yml 
 kubectl create -f example-app.yml 
+
+kubectl apply --filename https://raw.githubusercontent.com/giantswarm/kubernetes-prometheus/master/manifests-all.yaml
