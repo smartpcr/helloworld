b805e5456acfad3eb8323a80956fff242dda963e
lingxd <lingxd@gmail.com>
2018-08-25 19:24:02 -0700

    devbox vm terraform

--- Scripts/Provision/vm/setup.ps1
+++ Scripts/Provision/vm/setup.ps1
@@ -14,6 +14,8 @@ if (!$vmProvisionFolder) {
 $provisionFolder = Split-Path $vmProvisionFolder -Parent
 $scriptFolder = Split-Path $provisionFolder -Parent
 $EnvFolder = Join-Path $scriptFolder "Env"
+$credentialFolder = Join-Path $envFolder "credential"
+$envCredentialFolder = Join-Path $credentialFolder $EnvName
 $moduleFolder = Join-Path $scriptFolder "modules"
 Import-Module (Join-Path $moduleFolder "common2.psm1") -Force
 Import-Module (Join-Path $moduleFolder "YamlUtil.psm1") -Force
@@ -23,37 +25,81 @@ SetupGlobalEnvironmentVariables -ScriptFolder $scriptFolder
 LogTitle "Setup devbox VM for Environment '$EnvName'"
 
 
-LogStep -Step 1 -Message "load environment yaml settings from Env/${EnvName}..."
+LogStep -Step 1 -Message "load environment yaml settings from Env '$EnvName'..."
 $bootstrapValues = Get-EnvironmentSettings -EnvName $EnvName -EnvRootFolder $EnvFolder
 $azAccount = LoginAzureAsUser2 -SubscriptionName $bootstrapValues.global.subscriptionName
 $vaultName = $bootstrapValues.kv.name
 $rgName = $bootstrapValues.global.resourceGroup
 $tenantId = $bootstrapValues.global.tenantId
+$subscriptionId = $azAccount.id
+$envCredFolder = Join-Path (Join-Path $EnvFolder "credential") $EnvName
+New-Item -Path $envCredFolder -ItemType Directory -Force | Out-Null
 
-$secretValueFile = Join-Path $EnvFolder "credential/$EnvName/azure_provider.tfvars"
-if (-not (Test-Path $secretValueFile)) {
-    New-Item -Path (Join-Path $EnvFolder "credential/$EnvName") -ItemType Directory -Force | Out-Null
-    New-Item -Path $secretValueFile -ItemType File -Force | Out-Null
+
+LogStep -Step 2 -Message "Ensure terraform service principal is setup..."
+$terraformSpn = Get-OrCreateServicePrincipalUsingPassword `
+    -ServicePrincipalName $bootstrapValues.terraform.servicePrincipal `
+    -ServicePrincipalPwdSecretName $bootstrapValues.terraform.servicePrincipalSecretName `
+    -VaultName $vaultName
+$terraformSpnPwd = Get-OrCreatePasswordInVault2 `
+    -VaultName $vaultName `
+    -SecretName $bootstrapValues.terraform.servicePrincipalSecretName
+az ad sp credential reset --name $bootstrapValues.terraform.servicePrincipal --password $terraformSpnPwd.value 
+
+LogInfo -Message "Ensure terraform service principal have correct permissions..."
+az role assignment create `
+    --assignee $terraformSpn.appId `
+    --role Contributor `
+    --scope "/subscriptions/$subscriptionId" | Out-Null
+az keyvault set-policy `
+    --name $vaultName `
+    --resource-group $bootstrapValues.kv.resourceGroup `
+    --object-id $terraformSpn.objectId `
+    --spn $terraformSpn.displayName `
+    --certificate-permissions get list update delete `
+    --secret-permissions get list set delete | Out-Null
+
+LogStep -Step 3 -Message "Setup Windows VM..."
+$winSecretValueFile = Join-Path $envCredFolder "devbox_win10.tfvars"
+if (-not (Test-Path $winSecretValueFile)) {
+    New-Item -Path $winSecretValueFile -ItemType File -Force | Out-Null
 }
-SetTerraformValue -valueFile $secretValueFile -name "subscription_id" -value $azAccount.id
-SetTerraformValue -valueFile $secretValueFile -name "tenant_id" -value $tenantId
+SetTerraformValue -valueFile $winSecretValueFile -name "subscription_id" -value $azAccount.id
+SetTerraformValue -valueFile $winSecretValueFile -name "tenant_id" -value $tenantId
+SetTerraformValue -valueFile $winSecretValueFile -name "client_id" $terraformSpn.appId 
+SetTerraformValue -valueFile $winSecretValueFile -name "client_secret" $terraformSpnPwd.value 
 
+LogInfo "Retrieve windows user password..."
+$devboxUserPwdSecretName = "admin-password"
+$password = Get-OrCreatePasswordInVault2 -VaultName $vaultName -SecretName $devboxUserPwdSecretName
+SetTerraformValue -valueFile $winSecretValueFile -name "admin_password" -value $password.value 
 
-LogStep -Step 2 -Message "Setup Windows VM..."
 $winProvisionFolder = Join-Path $vmProvisionFolder "win10"
 Set-Location $winProvisionFolder
-$winTfFile = Join-Path $winProvisionFolder "main.tf"
-SetTerraformValue -valueFile $winTfFile -name "resource_group_name" -value $rgName
 terraform init 
-terraform plan -var-file $secretValueFile
-terraform apply -var-file $secretValueFile
+terraform plan -var-file $winSecretValueFile
+terraform apply -var-file $winSecretValueFile
 
 
-LogStep -Step 3 -Message "Setup Ubuntu VM..."
+LogStep -Step 4 -Message "Setup Ubuntu VM..."
+$ubuntuSecretValueFile = Join-Path $envCredFolder "devbox_ubuntu.tfvars"
+if (-not (Test-Path $ubuntuSecretValueFile)) {
+    New-Item -Path $ubuntuSecretValueFile -ItemType File -Force | Out-Null
+}
+
+EnsureSshCert `
+    -VaultName $vaultName `
+    -CertName $devboxUserPwdSecretName `
+    -EnvName $EnvName `
+    -ScriptFolder $scriptFolder 
+$publicSshKeyFile = Join-Path $envCredentialFolder "$($devboxUserPwdSecretName).pub"
+SetTerraformValue -valueFile $ubuntuSecretValueFile -name "subscription_id" -value $azAccount.id
+SetTerraformValue -valueFile $ubuntuSecretValueFile -name "tenant_id" -value $tenantId
+SetTerraformValue -valueFile $ubuntuSecretValueFile -name "client_id" $terraformSpn.appId 
+SetTerraformValue -valueFile $ubuntuSecretValueFile -name "ssh_key_data" $publicSshKeyFile
+
 $ubuntuProvisionFolder = Join-Path $vmProvisionFolder "ubuntu1804"
 Set-Location $ubuntuProvisionFolder
-$ubuntuTfFile = Join-Path $ubuntuProvisionFolder "main.tf"
-SetTerraformValue -valueFile $ubuntuTfFile -name "resource_group_name" -value $rgName
 terraform init 
-terraform plan -var-file $secretValueFile
-terraform apply -var-file $secretValueFile
+terraform plan -var-file $ubuntuSecretValueFile
+terraform apply -var-file $ubuntuSecretValueFile
