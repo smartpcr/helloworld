664e8a576807266851b7c9dca0c2ac3892f66b0f
xiaodoli <xiaodoli@microsoft.com>
2019-04-11 18:03:11 -0700

    ok

--- Scripts/ConnectTo-AksCluster.ps1
+++ Scripts/ConnectTo-AksCluster.ps1
@@ -1,6 +1,7 @@
 param(
     [string] $EnvName = "dev",
-    [switch] $asAdmin
+    [switch] $AsAdmin,
+    [int] $Port = 8082
 )
 
 
@@ -17,14 +18,14 @@ Import-Module (Join-Path $moduleFolder "VaultUtil.psm1") -Force
 SetupGlobalEnvironmentVariables -ScriptFolder $scriptFolder
 $bootstrapValues = Get-EnvironmentSettings -EnvName $envName -EnvRootFolder $envFolder
 LoginAzureAsUser2 -SubscriptionName $bootstrapValues.global.subscriptionName | Out-Null
-if ($asAdmin) {
-    az aks get-credentials --resource-group $bootstrapValues.aks.resourceGroup --name $bootstrapValues.aks.clusterName --admin
+if ($AsAdmin) {
+    az.cmd aks get-credentials --resource-group $bootstrapValues.aks.resourceGroup --name $bootstrapValues.aks.clusterName --admin
 } 
 else {
-    az aks get-credentials --resource-group $bootstrapValues.aks.resourceGroup --name $bootstrapValues.aks.clusterName | Out-Null
+    az.cmd aks get-credentials --resource-group $bootstrapValues.aks.resourceGroup --name $bootstrapValues.aks.clusterName | Out-Null
 }
 
-$kubeContextName = "$(kubectl config current-context)" 
+$kubeContextName = "$(kubectl.exe config current-context)" 
 Write-Host "You are now connected to kubenetes context: '$kubeContextName'" -ForegroundColor Green
 
 Write-Host "Browse aks dashboard..." -ForegroundColor Green
@@ -37,4 +38,4 @@ if ($answer2 -ieq "n") {
     return 
 }
 
-az aks browse --resource-group $bootstrapValues.aks.resourceGroup --name $bootstrapValues.aks.clusterName
+az aks browse --resource-group $bootstrapValues.aks.resourceGroup --name $bootstrapValues.aks.clusterName --listen-port $Port 
